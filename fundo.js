899aa99d2aaf006421f189c1381ba7a7bb7e8f45
JHONNYS MAX SAMPAIO DA SILVA <jhonnysmax2@gmail.com>
2026-03-31 00:02:16 -0300

    Add files via upload

---
+++ fundo.js
@@ -0,0 +1,14 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#00bcd4" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    line_linked: {
+      enable: true,
+      color: "#00bcd4"
+    }
+  }
+});
