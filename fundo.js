4c6eff6385c60a1beddc6d7cce8cf5d29aefdb38
JHONNYS MAX SAMPAIO DA SILVA <jhonnysmax2@gmail.com>
2026-03-31 11:58:12 -0300

    Refactor menu toggle event listeners
    Refactor menu toggle functionality for clarity.

--- fundo.js
+++ fundo.js
@@ -8,14 +8,16 @@ toggle.addEventListener("click", () => {
   menu.classList.toggle("ativo");
 });
 
-// 🔥 FECHAR AO CLICAR EM UM LINK
+// fechar ao clicar em um link
 links.forEach(link => {
   link.addEventListener("click", () => {
     menu.classList.remove("ativo");
     toggle.classList.remove("ativo");
-
+  });
 });
- document.addEventListener("click", (e) => {
+
+// fechar ao clicar fora
+document.addEventListener("click", (e) => {
   if (!menu.contains(e.target) && !toggle.contains(e.target)) {
     menu.classList.remove("ativo");
     toggle.classList.remove("ativo");
